9e47efa1d75c6232492f131a952c4d3dae7e84bc
SsangWoo <tkddn204@gmail.com>
2019-03-06 13:59:07 +0900

    copy 기능 변경

--- src/samsungext.js
+++ src/samsungext.js
@@ -4,7 +4,6 @@ $(document).ready(() => {
     parallel(Object.keys(STORE), (key, cb) => store.setIfNull(STORE[key], DEFAULTS[key], cb), loadExtension);
 
     function loadExtension() {
-
         enableContextMenu();
         updateHeader();
         updateMyPage();
